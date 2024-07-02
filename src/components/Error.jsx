24dbef71ea21b124a6732f4d0e0b78a3ae57b4b1
muhammad adhil <muhammadadhil934@gmail.com>
2024-07-02 19:38:37 +0530

    .

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,13 @@
 
+
+import { useRouteError } from 'react-router-dom';
 import React from 'react'
 
 const Error = () => {
+
+    const error=useRouteError();              //? useRouteError is a hook provided by react router dom 
+    console.log(error);
+    
   return (
       <div>
           <h1>oops..</h1>
